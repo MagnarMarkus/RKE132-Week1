ab7fe8d3215069188e9daf2106e420db267d00c8
magnar <magnar-markus.lill@tktk.ee>
2024-05-10 18:06:49 +0300

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,8 @@
+﻿// this is a comment to my code
+//app küsib kasutajalt nime
+//tervitab kasutajat
+Console.WriteLine("Enter your name: "); //output
+string userName;
+
+userName=Console.ReadLine(); //input
+Console.WriteLine($"Hello, {userName}!");  //output
